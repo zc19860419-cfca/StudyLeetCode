5454478f15c0cd18757d0bcbc7a8ba2865d8d73b
zhangchong <509811327@qq.com>
2020-05-10 11:38:13 +0800

    1.复习70 增加注释 爬楼梯

--- src/main/java/com/study/leetCode/dynamicProgramming/ClimbStairsSolution.java
+++ src/main/java/com/study/leetCode/dynamicProgramming/ClimbStairsSolution.java
@@ -3,32 +3,27 @@ package com.study.leetCode.dynamicProgramming;
 /**
  * @Author: zhangchong
  * @Description: 70. 爬楼梯
- * You are climbing a stair case. It takes n steps to reach to the top.
+ * 假设你正在爬楼梯。需要 n 阶你才能到达楼顶。
  * <p>
- * Each time you can either climb 1 or 2 steps. In how many distinct ways can yo
- * u climb to the top?
+ * 每次你可以爬 1 或 2 个台阶。你有多少种不同的方法可以爬到楼顶呢？
  * <p>
- * Note: Given n will be a positive integer.
+ * 注意：给定 n 是一个正整数。
  * <p>
- * Example 1:
+ * 示例 1：
  * <p>
+ * 输入： 2
+ * 输出： 2
+ * 解释： 有两种方法可以爬到楼顶。
+ * 1.  1 阶 + 1 阶
+ * 2.  2 阶
+ * 示例 2：
  * <p>
- * Input: 2
- * Output: 2
- * Explanation: There are two ways to climb to the top.
- * 1. 1 step + 1 step
- * 2. 2 steps
- * <p>
- * <p>
- * Example 2:
- * <p>
- * <p>
- * Input: 3
- * Output: 3
- * Explanation: There are three ways to climb to the top.
- * 1. 1 step + 1 step + 1 step
- * 2. 1 step + 2 steps
- * 3. 2 steps + 1 step
+ * 输入： 3
+ * 输出： 3
+ * 解释： 有三种方法可以爬到楼顶。
+ * 1.  1 阶 + 1 阶 + 1 阶
+ * 2.  1 阶 + 2 阶
+ * 3.  2 阶 + 1 阶
  * <p>
  * Related Topics 动态规划
  */
@@ -43,9 +38,13 @@ public class ClimbStairsSolution {
         } else {
             //mem[i] 表示从顶层 n 下 i+1 层总的走法个数
             int[] mem = new int[n];
-            mem[0] = 1;
-            mem[1] = 2;
+            mem[0] = 1; //从顶层 n 下 1 层只有一种解法
+            mem[1] = 2;//从顶层 n 下 2 层有2种解法 ：1 阶 + 1 阶 或者 2阶
             for (int i = 2; i < n; i++) {
+                //从顶层 n 下 i+1 层总的走法个数=从顶层 n 下 i 层总的走法个数+从顶层 n 下 i-1 层总的走法个数
+                //      __  i-1
+                //   __|    i
+                //__|       i+1：能从i层下来 也能从 i-1层下来
                 mem[i] = mem[i - 1] + mem[i - 2];
             }
             ans = mem[n - 1];
