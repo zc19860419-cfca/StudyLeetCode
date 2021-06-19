24d491be22a225ff1784cf97b0074a37f3b32619
zhangchong <509811327@qq.com>
2021-06-19 18:46:05 +0800

    33. 搜索旋转排序数组

--- src/main/java/com/study/leetCode/binarySearch/SearchInRotatedArraySolution33.java
+++ src/main/java/com/study/leetCode/binarySearch/SearchInRotatedArraySolution33.java
@@ -15,20 +15,27 @@ public class SearchInRotatedArraySolution33 {
         }
         int l = 0, r = n - 1;
         while (l <= r) {
+            //当前搜素区间 [l, r]
             int mid = (l + r) / 2;
             if (nums[mid] == target) {
                 return mid;
             }
             if (nums[0] <= nums[mid]) {
+                //说明[0, mid] 是升序区间，可以进行二分搜索
                 if (nums[0] <= target && target < nums[mid]) {
+                    // target in [0, mid]->下次搜素区间 ==>[l, mid-1]
                     r = mid - 1;
                 } else {
+                    // target in [mid+1, r]->下次搜素区间 ==>[mid+1, r]
                     l = mid + 1;
                 }
             } else {
+                //说明[mid,r] 是升序区间，可以进行二分搜索
                 if (nums[mid] < target && target <= nums[n - 1]) {
+                    // target in [mid, n-1]->下次搜素区间 ==>[mid+1, r]
                     l = mid + 1;
                 } else {
+                    // target in [0, mid-1]->下次搜素区间 ==>[l, mid-1]
                     r = mid - 1;
                 }
             }
